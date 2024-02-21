b294748de16e17a22e99bf1dd1811e3c3560e764
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-21 10:59:53 -0700

    registered in dbcontext

--- server/src/db/DbContext.js
+++ server/src/db/DbContext.js
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose'
 import { AccountSchema } from '../models/Account'
 import { ValueSchema } from '../models/Value'
+import { AnimalSchema } from '../models/Animal.js';
 
 class DbContext {
   Values = mongoose.model('Value', ValueSchema);
   Account = mongoose.model('Account', AccountSchema);
+
+  Animals = mongoose.model('Animal', AnimalSchema)
 }
 
 export const dbContext = new DbContext()
