89b38647ad2f04eef2d9b758d19f82382efd8846
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-21 14:07:24 -0700

    populating animal on create

--- server/src/services/ShowAnimalsService.js
+++ server/src/services/ShowAnimalsService.js
@@ -3,6 +3,7 @@ import { dbContext } from "../db/DbContext.js"
 class ShowAnimalsService {
   async createShowAnimal(showAnimalData) {
     const showAnimal = await dbContext.ShowAnimals.create(showAnimalData)
+    await showAnimal.populate('animal')
     return showAnimal
   }
 }
