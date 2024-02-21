2bf3b7fa1ac4810c5bf4e2014ce78cb5ccda2244
Jeremy Fowler <jembyfowler@gmail.com>
2024-02-21 14:11:29 -0700

    populating show

--- server/src/services/ShowAnimalsService.js
+++ server/src/services/ShowAnimalsService.js
@@ -4,6 +4,7 @@ class ShowAnimalsService {
   async createShowAnimal(showAnimalData) {
     const showAnimal = await dbContext.ShowAnimals.create(showAnimalData)
     await showAnimal.populate('animal')
+    await showAnimal.populate('show')
     return showAnimal
   }
 }
